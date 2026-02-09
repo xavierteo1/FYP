161cce9948dfbbc04abb298d4f0fcc2261b3f1cc
Xavier Teo <xavierteo1@github.com>
2026-02-09 13:32:57 +0800

    Add Railway MySQL database configuration using environment variables

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const mysql = require('mysql2');
 
+require('dotenv').config();
+
 //Database connection details
 const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Republic_C207',
-    database: 'clothes_swap'
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
   });
 
 //Connecting to database
